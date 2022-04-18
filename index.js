4d5d5efa808e6d0982dc32777776277207bb35df
DanilDoubleYou <danildoubleyou@gmail.com>
2022-04-18 13:31:29 +0500

    auth0 middleware added

--- index.js
+++ index.js
@@ -1,40 +1,60 @@
+//express config
 const { response } = require("express");
 const express = require("express");
 const app = express();
 
+//.env config
+require("dotenv").config();
+
+//auth0 config
+const { auth } = require('express-openid-connect');
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUER
+};
+
+//host config
 const host = 'localhost'
 const port = 7000
 
-//request info (middleware)
-app.use(function(request, response, next){
-     
+//auth0 middleware
+app.use(auth(config));
+
+//request user info (middleware)
+app.use(function(req, res, next){
+  
+  console.log(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
   let now = new Date();
   let hour = now.getHours();
   let minutes = now.getMinutes();
   let seconds = now.getSeconds();
-  let data = `${hour}:${minutes}:${seconds} ${request.method} ${host}:${port}${request.url} request`; //${request.get("user-agent")}
+  let data = `${hour}:${minutes}:${seconds} ${req.method} ${host}:${port}${req.url} request`; //${request.get("user-agent")}
   console.log(data);
   next();
 });
 
 //404 test page
-app.get('/home/foo', function(request, response){
-    response.sendStatus(404);
+app.get('/home/foo', function(req, res){
+    res.sendStatus(404);
 });
 
 //info page
-app.get("/info/", function(request, response){
-    response.send("Info Page!");
+app.get("/info/", function(req, res){
+    res.send("Info Page!");
   });
 
 //home page
-app.get("/home/", function(request, response){
-  response.send("Home Page!");
+app.get("/home/", function(req, res){
+  res.send("Home Page!");
 });
 
 //main page
-app.get("/", function(request, response){
-    response.send("Main Page!");
+app.get("/", function(req, res){
+    res.send("Main Page!");
 });
 
 //server start
