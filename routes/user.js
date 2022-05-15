dccaf1324e125f816eb196da83af01cccd839a22
DanilDoubleYou <danildoubleyou@gmail.com>
2022-05-16 04:23:08 +0500

    getMethod added

--- routes/user.js
+++ routes/user.js
@@ -2,4 +2,19 @@ import Router from "express"
 import User from "../models/User.js"
 const userRouter = new Router()
 
+//get a user
+userRouter.get("/", async (req, res) => {
+    const userId = req.query.userId;
+    const username = req.query.username;
+    try {
+      const user = userId
+        ? await User.findById(userId)
+        : await User.findOne({ username: username });
+      const { password, updatedAt, ...other } = user._doc;
+      res.status(200).json(other);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 export default userRouter
