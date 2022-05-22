34fc46b5730800b28820b05c4f02ed23fd19151a
DanilDoubleYou <danildoubleyou@gmail.com>
2022-05-22 06:17:47 +0500

    default avatar is now provided

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const UserSchema = new Schema({
     lastName : { required: true, type: String},
     email : { required: true, unique: true, type: String},
     password: { required: true, type: String},
-    avatar: { type: String }
+    avatar: { type: String, default: "https://media.istockphoto.com/vectors/user-icon-flat-isolated-on-white-background-user-symbol-vector-vector-id1300845620?k=20&m=1300845620&s=612x612&w=0&h=f4XTZDAv7NPuZbG0habSpU0sNgECM0X7nbKzTUta3n8="}
 }, {timestamps: true})
 
 export default model('User', UserSchema)
